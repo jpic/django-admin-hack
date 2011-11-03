c12a1811496ec99897fdf97caf6c8d917ba9b40e
James Pic <jamespic@gmail.com>
2011-11-03 13:52:02 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-admin-tools-plus',
+    version="0.0",
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    description='Manage menus and fieldsets throught the admin',
+    url='http://github.com/jpic/django-admin-tools-plus',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+    requires=[
+        'django-mptt>=0.5',
+        'django-admin-tools',
+    ],
+)
