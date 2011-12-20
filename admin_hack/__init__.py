5588468fafa467c6044dd9d22396470e52525c21
jpic <jamespic@gmail.com>
2011-12-20 12:18:59 +0100

    Fugbix: patcher function was not aware of the url change from hack/js/ to hack.js

--- admin_hack/__init__.py
+++ admin_hack/__init__.py
@@ -28,7 +28,7 @@ def patch_admin(site, admin_hack_prefix='/admin_hack/'):
     for model, options in site._registry.items():
         # smelly code, obfuscation over metaclass FTW
         media = options.media
-        media.add_js(['%sjs/' % admin_hack_prefix])
+        media.add_js(['%shack.js' % admin_hack_prefix])
         media.add_css({
             'all': [settings.STATIC_URL + 'admin_hack/style.css']
         })
