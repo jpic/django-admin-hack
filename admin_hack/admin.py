214694f9bf3a49e686a762203ab18ca9dedc7ae1
jpic <jamespic@gmail.com>
2012-01-23 02:43:25 +0100

    Removed useless admin

--- admin_hack/admin.py
+++ admin_hack/admin.py
@@ -7,37 +7,4 @@ class CustomValueInline(admin.TabularInline):
     model = CustomValue
     template = 'admin_hack/customvalue_tabularinline.html'
 
-class FieldInline(admin.TabularInline):
-    model = Field
-    extra = 0
-
-class FormModelAdmin(admin.ModelAdmin):
-    inlines = (
-        FieldInline,
-    )
-
-    def get_formsets(self, request, obj=None):
-        if obj is not None:
-            for i in super(FormModelAdmin, self).get_formsets(request, obj):
-                yield i
-
-    def save_model(self, request, obj, form, change):
-        if not obj.pk:
-            fields = obj.model_admin().get_form(request).base_fields
-
-            # save a 'full' preset with all fields
-            if Form.objects.filter(contenttype=obj.contenttype).count() == 0:
-                form = Form(name='full', contenttype=obj.contenttype)
-                form.save()
-                for name, field in fields.items():
-                    Field(form=form, name=name).save()
-
-            obj.save()
-
-            for name, field in fields.items():
-                Field(form=obj, name=name).save()
-        else:
-            obj.save()
-
 admin.site.register(AdminHackUserProfile)
-admin.site.register(Form, FormModelAdmin)
